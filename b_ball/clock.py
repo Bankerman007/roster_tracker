e0326e59bf76447e66b2f8bdbce674cf3e985e41
Scott Wright <scottwright.qrf@gmail.com>
2022-01-01 17:06:11 -0600

    add email and cell to player

--- b_ball/clock.py
+++ b_ball/clock.py
@@ -2,8 +2,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='tue', hour=23)
+@sched.scheduled_job('cron', day_of_week='fri', hour=23)
 def scheduled_job():
+    pass
     print('This job is run every Tuesday at 11pm.')
 
 sched.start()
--- b_ball/forms.py
+++ b_ball/forms.py
@@ -6,5 +6,7 @@ from .models import Player
 class PlayerForm(ModelForm):
     class Meta:
         model = Player
-        fields = ['player_name',]
+        fields = ['player_name','player_email','player_cell']
         player_name = forms.CharField()
+        player_email = forms.CharField()
+        player_cell = forms.CharField()

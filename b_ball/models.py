f6e4668be81b83ecdcb10c09f19dcfab425dfce4
Scott Wright <scottwright.qrf@gmail.com>
2022-01-02 12:57:00 -0600

    altered defaul on db

--- b_ball/models.py
+++ b_ball/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Player(models.Model):
     player_name = models.CharField(max_length=30)
-    player_email = models.CharField(max_length=30, default=123)
-    player_cell = models.CharField(max_length=30, default=123)
+    player_email = models.CharField(max_length=30)
+    player_cell = models.CharField(max_length=30)
     
     def __str__(self):
         return self.player_name

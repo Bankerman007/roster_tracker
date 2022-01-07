946f3c4d7daa1986ed18e2139a57cb9e2d651b75
Scott Wright <scottwright.qrf@gmail.com>
2022-01-07 15:28:30 -0600

    remove email field

--- b_ball/forms.py
+++ b_ball/forms.py
@@ -6,7 +6,7 @@ from .models import Player
 class PlayerForm(ModelForm):
     class Meta:
         model = Player
-        fields = ['player_name','player_email','player_cell']
+        fields = ['player_name','player_cell']
         player_name = forms.CharField()
         player_email = forms.CharField()
         player_cell = forms.CharField()

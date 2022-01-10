2dffb958af972797d6cc5293ce844b37f710bb1d
Scott Wright <scottwright.qrf@gmail.com>
2022-01-09 21:05:54 -0600

    update script calls

---
+++ scripts/clock.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from b_ball.reset_wkly_db import delete_players
+from b_ball.save_to_db import start_db_save
+from multiprocessing import Process
+
+
+
+def run(*args):
+    today = datetime.today()
+    day = today.weekday()
+    if day == 1:
+        start_db_save()  #('This job runs every Tuesday night at 11pm')
+        if day == 2:
+            delete_players()  #('This job runs every Wednesday night at 11pm')
+           
---
+++ scripts/clock2.py
@@ -0,0 +1,18 @@
+from datetime import date, datetime
+from b_ball.sms_all import sms_all
+from b_ball.sms_reminder import sms_reminder
+
+
+def run(*args):
+    today = datetime.today()
+    day = today.weekday()
+    if day == 0:
+        sms_all()  #('This job is run every Monday at 10am.')
+        if day == 1:
+            sms_reminder()  #('This job is run every Tuesday at 10am.')
+        else:
+            pass
+    else:
+        pass
+    
+

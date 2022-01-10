02b60355c8eee8d72fb1ab6a151caed8eb9742ca
Scott Wright <scottwright.qrf@gmail.com>
2022-01-10 11:18:43 -0600

    changes

--- roster/settings.py
+++ roster/settings.py
@@ -16,7 +16,7 @@ import os.path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_ROOT = os.path.normpath(os.path.dirname(__file__))
 
 

606289c62b305751d729d9c200e22dd05931081b
Scott Wright <scottwright.qrf@gmail.com>
2022-01-10 11:48:13 -0600

    changes

--- roster/settings.py
+++ roster/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'b_ball/static')
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates', 'static', 'staticfiles'))
+TEMPLATE_DIRS = (os.path.join(BASE_DIR,'templates', 'static', 'staticfiles')),
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 

eb9845b19161c0408bbcf15664c859df370c80f6
Scott Wright <scottwright.qrf@gmail.com>
2022-01-10 12:01:29 -0600

    changes

--- b_ball/sms_reminder.py
+++ b_ball/sms_reminder.py
@@ -1,6 +1,6 @@
 import os
 from twilio.rest import Client
-from b_ball.save_to_db import save_to_db
+from b_ball.models import Player
 
 
 # Find your Account SID and Auth Token at twilio.com/console
@@ -10,7 +10,16 @@ def sms_reminder():
     account_sid = 'AC7d0ce7af110c2d84da65e506722c1f2d'
     auth_token = '21d2b9c70be435ee1a1869a1f452e145'
     client = Client(account_sid, auth_token)
-    [twl_value] = save_to_db()
+    this_weeks_players_dict = Player.objects.values('player_name', 'player_cell')
+    this_weeks_list = []
+
+    for ele in this_weeks_players_dict:
+        for key, value in ele.items():
+            v = value
+            this_weeks_list.append(v)
+
+    twl = {this_weeks_list[i]: this_weeks_list[i + 1] for i in range(0, len(this_weeks_list), 2)}
+    twl_value = list(twl.values())
     tw_players = twl_value
     for p in tw_players:
         send_cell = p

6a2631c39cd32e7237373cb6424406b59c4bd872
Scott Wright <scottwright.qrf@gmail.com>
2022-01-11 13:25:31 -0600

    changes

--- b_ball/sms_all.py
+++ b_ball/sms_all.py
@@ -27,7 +27,7 @@ def sms_all():
 
         message = client.messages \
                         .create(
-                             body="Ballers Needed! Register for ball this weeek: b-ball-app.herokuapp.com/register_player/",
+                             body="No Ball This Week. Gym closed.",
                              from_='+16467989631',
                              to= '+1'+ send_cell,
                     )

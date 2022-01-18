b5e755cdfea7bbca08639d30ebc07f5acf141e8b
Scott Wright <scottwright.qrf@gmail.com>
2022-01-18 13:09:09 -0600

    change

--- scripts/clock2.py
+++ scripts/clock2.py
@@ -6,9 +6,9 @@ from b_ball.sms_reminder import sms_reminder
 def run(*args):
     today = datetime.today()
     day = today.weekday()
-    if day == 1:
-        sms_all()  #('This job is run every Monday at 10am.')
     if day == 0:
+        sms_all()  #('This job is run every Monday at 10am.')
+    if day == 1:
         sms_reminder()  #('This job is run every Tuesday at 10am.')
 
     

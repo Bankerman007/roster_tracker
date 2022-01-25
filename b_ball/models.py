5c94d6f532d6e4d5393c6be00b8204dc2dd55567
Scott Wright <scottwright.qrf@gmail.com>
2022-01-25 16:07:00 -0600

    changes

--- b_ball/models.py
+++ b_ball/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Player(models.Model):
     player_name = models.CharField(max_length=30)
     player_email = models.CharField(max_length=30)
-    player_cell = models.CharField(max_length=30)
+    player_cell = models.CharField(max_length=30, default= "no spaces or dashes")
     
     def __str__(self):
         return self.player_name

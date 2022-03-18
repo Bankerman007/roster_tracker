7ae6d7de66c90407d3f76113a9f2028fd1d49ddd
Scott Wright <scottwright.qrf@gmail.com>
2022-03-17 20:51:23 -0500

    changes

--- roster/settings.py
+++ roster/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=(j$7eglsj4dq7ifi1b4%=-)ox_8c2!ilqd1noyn^1bx-)7l-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ['DEBUG']
+    
 
 ALLOWED_HOSTS = ["127.0.0.1", "b-ball-app.herokuapp.com"]
 

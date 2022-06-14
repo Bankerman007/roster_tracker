62b8d6a047c30e662c6bd930c86416072ef6fabf
Scott Wright <scottwright.qrf@gmail.com>
2022-06-14 09:33:45 -0500

    changes

--- scripts/clock2.py
+++ scripts/clock2.py
@@ -4,19 +4,19 @@ from b_ball.sms_all import sms_all
 from b_ball.sms_reminder import sms_reminder
 
 
-# def run(*args):
-#     today = datetime.today()
-#     day = today.weekday()
-#     now = datetime.now()
-#     current_hour = int(now.strftime("%H%M"))
+def run(*args):
+    today = datetime.today()
+    day = today.weekday()
+    now = datetime.now()
+    current_hour = int(now.strftime("%H%M"))
     
-#     if day == 0 and current_hour >= 1055 and current_hour <= 1105:
-#         sms_all()  #This job is run every Monday at 11am.
+    #if day == 0 and current_hour >= 1055 and current_hour <= 1105:
+    sms_all()  #This job is run every Monday at 11am.
     
-#     if day == 1 and current_hour >= 1155 and current_hour <= 1205:
-#         players_needed() #This job is run every Tuesday at 11am.
+    # if day == 1 and current_hour >= 1155 and current_hour <= 1205:
+    #     players_needed() #This job is run every Tuesday at 11am.
     
-#     if day == 1 and current_hour >= 1355 and current_hour <= 1410:
-#         sms_reminder()  #This job is run every Tuesday at 2pm.
+    # if day == 1 and current_hour >= 1355 and current_hour <= 1410:
+    #     sms_reminder()  #This job is run every Tuesday at 2pm.
     
 

ecde707c1662d82b100ef857680bf42f1db07bde
Scott Wright <scottwright.qrf@gmail.com>
2022-08-07 18:25:56 -0500

    changes

--- scripts/clock.py
+++ scripts/clock.py
@@ -5,14 +5,14 @@ from b_ball.models import TurnOff
 
 
 def run(*args):
-    flag = TurnOff.objects.all(pk=2)
-    test = flag.on_off
+    #flag = TurnOff.objects.all(pk=2)
+    #test = flag.on_off
     today = datetime.today()
     day = today.weekday()
     
-    if test == True:
-        if day == 1:
-            start_db_save()  #('This job runs every Tuesday night at 11pm')
-        if day == 2:
-            delete_players()  #('This job runs every Wednesday night at 11pm')
+    
+    if day == 1:
+        start_db_save()  #('This job runs every Tuesday night at 11pm')
+    if day == 2:
+        delete_players()  #('This job runs every Wednesday night at 11pm')
            
--- scripts/clock2.py
+++ scripts/clock2.py
@@ -11,22 +11,19 @@ def run(*args):
     day = today.weekday()
     now = datetime.now()
     current_hour = int(now.strftime("%H%M"))
-    flag = TurnOff.objects.all(pk=2)
-    test = flag.on_off
+    #flag = TurnOff.objects.all(pk=2)
+    #test = flag.on_off
     
-    if test == True:
-
-        if day == 6:
-            sms_reminder()
+   
     
-        if day == 0 and current_hour >= 1055 and current_hour <= 1105:
-            sms_all()  #This job is run every Monday at 11am.
+    if day == 0 and current_hour >= 1055 and current_hour <= 1105:
+        sms_all()  #This job is run every Monday at 11am.
 
-        if day == 0 and current_hour >= 1755 and current_hour <= 1805:
-            sms_pay_reminder()  #This job is run every Monday at 6pm.    
-        
-        if day == 1 and current_hour >= 1055 and current_hour <= 1105:
-            players_needed() #This job is run every Tuesday at 11am.
-        
-        if day == 1 and current_hour >= 1400 and current_hour <= 1405:
-            sms_reminder()  #This job is run every Tuesday at 2pm.
+    if day == 0 and current_hour >= 1755 and current_hour <= 1805:
+        sms_pay_reminder()  #This job is run every Monday at 6pm.    
+    
+    if day == 1 and current_hour >= 1055 and current_hour <= 1105:
+        players_needed() #This job is run every Tuesday at 11am.
+    
+    if day == 1 and current_hour >= 1400 and current_hour <= 1405:
+        sms_reminder()  #This job is run every Tuesday at 2pm.

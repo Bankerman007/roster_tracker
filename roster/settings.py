6b57b6d1dc269740450f6a3ae3e6ac98094f859a
Scott Wright <scottwright.qrf@gmail.com>
2022-12-02 19:57:17 -0600

    changes

--- roster/settings.py
+++ roster/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'roster.wsgi.application'
 
 DATABASES = {
     'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2'',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'bball',
     'USER':'nsszpmpp',
     'PASSWORD':'FPIg65p6hGj5Vg4oOYK-avmhndsWQyNt', 

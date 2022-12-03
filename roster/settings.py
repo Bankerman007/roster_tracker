a9b5af74b4971e5aa15d1989e774b981e22ba32c
Scott Wright <scottwright.qrf@gmail.com>
2022-12-03 08:04:14 -0600

    changes

--- roster/settings.py
+++ roster/settings.py
@@ -82,25 +82,26 @@ WSGI_APPLICATION = 'roster.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     'NAME': 'bball',
+#     'USER':'nsszpmpp',
+#     'PASSWORD':'FPIg65p6hGj5Vg4oOYK-avmhndsWQyNt', 
+#     'HOST':'db.elephantsql.com',
+#     'PORT': 5432
+#     }
+#}
 DATABASES = {
-    'default': {
-    'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    'NAME': 'bball',
-    'USER':'nsszpmpp',
-    'PASSWORD':'FPIg65p6hGj5Vg4oOYK-avmhndsWQyNt', 
-    'HOST':'db.elephantsql.com',
-    'PORT': 5432
-    }
-}
-#  {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-   
+        'default':{
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
 
-# }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+}
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

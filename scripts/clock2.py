364ee25aa06ad56639a5f293a5f4a8757fbf8551
Scott Wright <scottwright.qrf@gmail.com>
2022-12-04 16:50:50 -0600

    changes

--- scripts/clock2.py
+++ scripts/clock2.py
@@ -11,24 +11,24 @@ def run(*args):
     day = today.weekday()
     now = datetime.now()
     current_hour = int(now.strftime("%H%M"))
-    #flag = TurnOff.objects.get(pk=1)
-    #should_run = flag.on_off
-    sms_reminder()
+    flag = TurnOff.objects.get(pk=1)
+    should_run = flag.on_off
+    #sms_reminder()
     
-    # if should_run:
-    #     print('should run is true, excuting script')
+    if should_run:
+        print('should run is true, excuting script')
         
-    #     if day == 0 and current_hour >= 1055 and current_hour <= 1105:
-    #         sms_all()  #This job is run every Monday at 11am.
+        if day == 0 and current_hour >= 1055 and current_hour <= 1105:
+            sms_all()  #This job is run every Monday at 11am.
 
-    #     # if day == 0 and current_hour >= 1755 and current_hour <= 1805:
-    #     #     sms_pay_reminder()  #This job is run every Monday at 6pm.    
+        # if day == 0 and current_hour >= 1755 and current_hour <= 1805:
+        #     sms_pay_reminder()  #This job is run every Monday at 6pm.    
         
-    #     if day == 0 and current_hour >= 1655 and current_hour <= 1705:
-    #         players_needed() #This job is run every Monday at 5pm.
+        if day == 0 and current_hour >= 1655 and current_hour <= 1705:
+            players_needed() #This job is run every Monday at 5pm.
         
-    #     if day == 1 and current_hour >= 1400 and current_hour <= 1405:
-    #         sms_reminder()  #This job is run every Tuesday at 2pm.
+        if day == 1 and current_hour >= 1400 and current_hour <= 1405:
+            sms_reminder()  #This job is run every Tuesday at 2pm.
 
-    # else:
-    #     print('is not true, not executing script')
+    else:
+        print('is not true, not executing script')
